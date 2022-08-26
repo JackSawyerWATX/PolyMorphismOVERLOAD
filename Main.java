b7ed0c2e86eaa2f0d85c879af69a23cf2c958e75
Jack Sawyer <84543199+JackSawyerWATX@users.noreply.github.com>
2022-08-26 12:03:46 -0700

    changed the double integers for a more even outcome.

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ class Main {
     public static void main (String[] args){
         add(1,2);
         add(3,4,5);
-        add(6.2, 7.4, 8.6);
+        add(6.1, 7.2, 8.3);
     }
     public static void add(int first, int second){
         System.out.println(first+second);
